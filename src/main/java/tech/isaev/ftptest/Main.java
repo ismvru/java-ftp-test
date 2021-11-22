b9a7183431cf72cba9752965a76c5e44aa38c2cd
Mikhail Isaev <admin@isaev.tech>
2021-11-23 00:26:38 +0300

    Rename some_variables to someVariables

--- src/main/java/tech/isaev/ftptest/Main.java
+++ src/main/java/tech/isaev/ftptest/Main.java
@@ -16,30 +16,32 @@ public class Main {
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String STATUS_FAILED = ANSI_RED + "FAILED" + ANSI_RESET;
+    public static final String STATUS_OK = ANSI_GREEN + "OK" + ANSI_RESET;
 
     // Plain FTP
     private static void ftp(String server, Integer port, String user, String password) throws IOException {
-        FTPClient ftp_client = new FTPClient();
-        ftp_client.connect(server, port);
-        ftp_client.login(user, password);
-        ftp_client.disconnect();
+        FTPClient ftpClient = new FTPClient();
+        ftpClient.connect(server, port);
+        ftpClient.login(user, password);
+        ftpClient.disconnect();
     }
 
     // FTP with SSL
     private static void ftps(String server, Integer port, String user, String password) throws IOException {
-        FTPSClient ftps_client = new FTPSClient(true);
-        ftps_client.connect(server, port);
-        ftps_client.login(user, password);
-        ftps_client.disconnect();
+        FTPSClient ftpsClient = new FTPSClient(true);
+        ftpsClient.connect(server, port);
+        ftpsClient.login(user, password);
+        ftpsClient.disconnect();
     }
 
     // SFTP (SSH)
     private static void sftp(String server, Integer port, String user, String password) throws IOException {
-        SSHClient sftp_client = new SSHClient();
-        sftp_client.addHostKeyVerifier(new PromiscuousVerifier());
-        sftp_client.connect(server, port);
-        sftp_client.authPassword(user, password);
-        sftp_client.close();
+        SSHClient sftpClient = new SSHClient();
+        sftpClient.addHostKeyVerifier(new PromiscuousVerifier());
+        sftpClient.connect(server, port);
+        sftpClient.authPassword(user, password);
+        sftpClient.close();
     }
 
     // Main class
@@ -68,24 +70,24 @@ public class Main {
         options.addOption(port);
 
         // Debug
-        Option debug_flag = new Option("d", "debug", false, "Enable debug output");
-        debug_flag.setRequired(false);
-        options.addOption(debug_flag);
+        Option debugFlag = new Option("d", "debug", false, "Enable debug output");
+        debugFlag.setRequired(false);
+        options.addOption(debugFlag);
 
         // Plain FTP only
-        Option ftp_flag = new Option("fo", "ftp-only", false, "Make request for plane FTP only");
-        ftp_flag.setRequired(false);
-        options.addOption(ftp_flag);
+        Option ftpFlag = new Option("fo", "ftp-only", false, "Make request for plane FTP only");
+        ftpFlag.setRequired(false);
+        options.addOption(ftpFlag);
 
         // FTPs only
-        Option ftps_flag = new Option("fso", "ftps-only", false, "Make request for FTPs only");
-        ftps_flag.setRequired(false);
-        options.addOption(ftps_flag);
+        Option ftpsFlag = new Option("fso", "ftps-only", false, "Make request for FTPs only");
+        ftpsFlag.setRequired(false);
+        options.addOption(ftpsFlag);
 
         // FTPs only
-        Option sftp_flag = new Option("sfo", "sftp-only", false, "Make request for SFTP only");
-        sftp_flag.setRequired(false);
-        options.addOption(sftp_flag);
+        Option sftpFlag = new Option("sfo", "sftp-only", false, "Make request for SFTP only");
+        sftpFlag.setRequired(false);
+        options.addOption(sftpFlag);
 
         // Parse options
         CommandLineParser parser = new DefaultParser();
@@ -94,24 +96,23 @@ public class Main {
         try {
             cmd = parser.parse(options, args);
         } catch (ParseException e) {
-            System.out.println(e.getMessage());
             formatter.printHelp("ftptest", options);
             System.exit(1);
         }
 
         // Get values - required
-        String ftp_server = cmd.getOptionValue("server");
-        String ftp_user = cmd.getOptionValue("user");
-        String ftp_password = cmd.getOptionValue("password");
+        String ftpServer = cmd.getOptionValue("server");
+        String ftpUser = cmd.getOptionValue("user");
+        String ftpPassword = cmd.getOptionValue("password");
 
         // Get optional port name, if not passed - default == 21
-        int ftp_port, sftp_port;
+        int ftpPort, sftpPort;
         try {
-            ftp_port = Integer.parseInt(cmd.getOptionValue("port-number"));
-            sftp_port = ftp_port;
+            ftpPort = Integer.parseInt(cmd.getOptionValue("port-number"));
+            sftpPort = ftpPort;
         } catch (java.lang.NumberFormatException e) {
-            ftp_port = 21;
-            sftp_port = 22;
+            ftpPort = 21;
+            sftpPort = 22;
         }
 
         // If we see -d key - enable debug. Need more debug.
@@ -120,56 +121,58 @@ public class Main {
         }
 
         // We can decide what to do.
-        boolean do_ftp = Boolean.TRUE, do_ftps = Boolean.TRUE, do_sftp = Boolean.TRUE;
+        boolean doFtp = Boolean.TRUE;
+        boolean doFtps = Boolean.TRUE;
+        boolean doSftp = Boolean.TRUE;
         if (cmd.hasOption("ftp-only")) {
-            do_ftps = Boolean.FALSE;
-            do_sftp = Boolean.FALSE;
+            doFtps = Boolean.FALSE;
+            doSftp = Boolean.FALSE;
         }
         if (cmd.hasOption("ftps-only")) {
-            do_ftp = Boolean.FALSE;
-            do_sftp = Boolean.FALSE;
+            doFtp = Boolean.FALSE;
+            doSftp = Boolean.FALSE;
         }
         if (cmd.hasOption("sftp-only")) {
-            do_ftp = Boolean.FALSE;
-            do_ftps = Boolean.FALSE;
+            doFtp = Boolean.FALSE;
+            doFtps = Boolean.FALSE;
         }
 
         // We need to init logger
         final Logger logger = LoggerFactory.getLogger(Main.class);
 
         // Main app work
-        logger.info("Connect as " + ftp_user + " to " + ftp_server + ":" + ftp_port + " FTP(s) and to " + ftp_server + ":" + sftp_port + " SFTP");
+        logger.info("Connect as " + ftpUser + " to " + ftpServer + ":" + ftpPort + " FTP(s) and to " + ftpServer + ":" + sftpPort + " SFTP");
 
         // Plain FTP
-        if (do_ftp) {
+        if (doFtp) {
             try {
                 logger.info("Plain FTP");
-                ftp(ftp_server, ftp_port, ftp_user, ftp_password);
-                logger.info("Plain FTP - " + ANSI_GREEN + "OK" + ANSI_RESET);
+                ftp(ftpServer, ftpPort, ftpUser, ftpPassword);
+                logger.info("Plain FTP - " + STATUS_OK);
             } catch (IOException e) {
-                logger.error("Plain FTP - " + ANSI_RED + "FAILED" + ANSI_RESET);
+                logger.error("Plain FTP - " + STATUS_FAILED);
                 logger.debug(Arrays.toString(e.getStackTrace()));
             }
         }
         // FTP with SSL/TLS
-        if (do_ftps) {
+        if (doFtps) {
             try {
                 logger.info("FTP with SSL/TLS");
-                ftps(ftp_server, ftp_port, ftp_user, ftp_password);
-                logger.info("FTP with SSL/TLS - " + ANSI_GREEN + "OK" + ANSI_RESET);
+                ftps(ftpServer, ftpPort, ftpUser, ftpPassword);
+                logger.info("FTP with SSL/TLS - " + STATUS_OK);
             } catch (IOException e) {
-                logger.error("FTP with SSL/TLS - " + ANSI_RED + "FAILED" + ANSI_RESET);
+                logger.error("FTP with SSL/TLS - " + STATUS_FAILED);
                 logger.debug(Arrays.toString(e.getStackTrace()));
             }
         }
         // SFTP
-        if (do_sftp) {
+        if (doSftp) {
             try {
                 logger.info("SFTP");
-                sftp(ftp_server, sftp_port, ftp_user, ftp_password);
-                logger.info("SFTP - " + ANSI_GREEN + "OK" + ANSI_RESET);
+                sftp(ftpServer, sftpPort, ftpUser, ftpPassword);
+                logger.info("SFTP - " + STATUS_OK);
             } catch (IOException e) {
-                logger.error("SFTP - " + ANSI_RED + "FAILED" + ANSI_RESET);
+                logger.error("SFTP - " + STATUS_FAILED);
                 logger.debug(Arrays.toString(e.getStackTrace()));
             }
         }
